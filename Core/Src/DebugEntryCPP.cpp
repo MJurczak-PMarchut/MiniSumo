0a5627e26ab3c3b8dd52b75e40d0989b023a9b0f
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2022-11-03 00:17:49 +0100

    Added dirty driver for line detectors

--- Core/Src/DebugEntryCPP.cpp
+++ Core/Src/DebugEntryCPP.cpp
@@ -6,14 +6,17 @@
  */
 #include "EntryCPP.hpp"
 
-#include "../../RobotDrivers/Configuration.h"
-#include "../../RobotDrivers/I-BUS/I-BUS.hpp"
-#include "../../RobotDrivers/L9960T/L9960T.hpp"
-#include "../../RobotDrivers/RobotSpecificDefines.hpp"
+#include "Configuration.h"
+#include "I-BUS/I-BUS.hpp"
+#include "L9960T.hpp"
+#include "RobotSpecificDefines.hpp"
+#include "LineDetectors.hpp"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
@@ -27,6 +30,8 @@ extern DMA_HandleTypeDef hdma_uart7_rx;
 
 void EmStop(void);
 
+uint32_t FL, FR, BL, BR;
+
 uint8_t pRx_Data[40] = {0};
 
 
@@ -37,12 +42,13 @@ L9960T MOTOR_CONTROLLERS[] = {
 		[MOTOR_RIGHT] = L9960T(MOTOR_RIGHT, &hspi2, &MainCommManager, TIM_CHANNEL_3, &htim4)};
 
 IBus RxController(&huart7, EmStop, pRx_Data, &hdma_uart7_rx);
+LineDetectors LDLineDetectors(4);
 //VL53L1X vl53l1x = VL53L1X(&hi2c1, &MainCommManager);
 MessageInfoTypeDef MsgInfo = {0};
 uint16_t distance = 0;
 
 void InitControllers(void);
-
+void InitLineDetectors(void);
 
 void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
 {
@@ -63,28 +69,25 @@ void main_cpp(void * pvParameters )
 {
 	MainCommManager.AttachCommInt(&hspi2);
 	InitControllers();
+	InitLineDetectors();
+//	HAL_ADC_Start_DMA(&hadc2, data, 2);
 	MOTOR_CONTROLLERS[MOTOR_LEFT].Enable();
 	MOTOR_CONTROLLERS[MOTOR_RIGHT].Enable();
 	MOTOR_CONTROLLERS[MOTOR_LEFT].SetMotorDirection(MOTOR_DIR_BACKWARD);
  	MOTOR_CONTROLLERS[MOTOR_RIGHT].SetMotorDirection(MOTOR_DIR_BACKWARD);
 
- 	MOTOR_CONTROLLERS[MOTOR_LEFT].SetMotorPowerPWM(999);
- 	MOTOR_CONTROLLERS[MOTOR_RIGHT].SetMotorPowerPWM(999);
+ 	MOTOR_CONTROLLERS[MOTOR_LEFT].SetMotorPowerPWM(0);
+ 	MOTOR_CONTROLLERS[MOTOR_RIGHT].SetMotorPowerPWM(0);
 	HAL_UARTEx_ReceiveToIdle_DMA(&huart7, pRx_Data, 40);
 	__HAL_DMA_DISABLE_IT(&hdma_uart7_rx, DMA_IT_HT);
  	 while(1)
 	{
- 		vTaskDelay(1000);
- 		MOTOR_CONTROLLERS[MOTOR_LEFT].SetMotorDirection(MOTOR_DIR_BACKWARD);
- 	 	MOTOR_CONTROLLERS[MOTOR_RIGHT].SetMotorDirection(MOTOR_DIR_BACKWARD);
- 	 	HAL_Delay(1000);
- 		MOTOR_CONTROLLERS[MOTOR_LEFT].SetMotorDirection(MOTOR_DIR_FORWARD);
- 	 	MOTOR_CONTROLLERS[MOTOR_RIGHT].SetMotorDirection(MOTOR_DIR_FORWARD);
-		if(huart7.RxState == HAL_UART_STATE_READY)
-		{
-			HAL_UARTEx_ReceiveToIdle_DMA(&huart7, pRx_Data, 40);
-			__HAL_DMA_DISABLE_IT(&hdma_uart7_rx, DMA_IT_HT);
-		}
+ 		vTaskDelay(10);
+ 		FL =LDLineDetectors.GetDetectorValue(FRONT_LEFT);
+ 		FR =LDLineDetectors.GetDetectorValue(FRONT_RIGHT);
+ 		BL =LDLineDetectors.GetDetectorValue(BACK_LEFT);
+ 		BR =LDLineDetectors.GetDetectorValue(BACK_RIGHT);
+
 	}
 }
 
@@ -110,6 +113,15 @@ void InitControllers(void)
 	while(MOTOR_CONTROLLERS[MOTOR_RIGHT].CheckIfControllerInitializedOk() != HAL_OK)
 	{}
 
+}
+
+void InitLineDetectors(void)
+{
+	LineDetectors_t LD_List[2] = {[0] = FRONT_LEFT, [1] = FRONT_RIGHT};
+	LDLineDetectors.AttachDetectors(&hadc1, LD_List, 2);
+	LD_List[0] = BACK_RIGHT;
+	LD_List[1] = BACK_LEFT;
+	LDLineDetectors.AttachDetectors(&hadc2, LD_List, 2);
 }
 
 #ifdef __cplusplus
